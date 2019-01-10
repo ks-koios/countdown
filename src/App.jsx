a4aa9cae2e768c50045a8a67c83247ac803ff4fa
Kevin Sidhu <sidhu.kevin@gmail.com>
2019-01-09 23:13:30 -0500

    Created App component with css

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2018'
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-title">Countdown to {this.state.deadline}</div>
+                <div>
+                    <div className="Clock-days">14 days</div>
+                    <div className="Clock-hours">30 hours</div>
+                    <div className="Clock-minutes">15 Minutes</div>
+                    <div className="Clock-seconds">20 seconds</div>
+                </div>
+                <div>
+                    <input placeholder="new date" />
+                    <button>Submit</button>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default App;
